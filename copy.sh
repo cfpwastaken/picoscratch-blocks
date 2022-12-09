c9d19669b2c1c696df9b03608ed696788fcbafbb
Cfp <cfp@cfp.gotdns.ch>
2022-12-09 18:55:40 +0100

    I dont even know

--- copy.sh
+++ copy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-cp blockly_compressed_vertical.js ../ -v
-cp blocks_compressed_vertical.js ../ -v
-cp blocks_compressed.js ../ -v
-#cp media ../ -rv
-cp msg/messages.js ../msg -v
-cp msg/scratch_msgs.js ../msg -v
+cp blockly_compressed_vertical.js ../picoscratch-learn/web -v
+cp blocks_compressed_vertical.js ../picoscratch-learn/web -v
+cp blocks_compressed.js ../picoscratch-learn/web -v
+#cp media ../picoscratch-learn/web -rv
+cp msg/messages.js ../picoscratch-learn/web/msg -v
+cp msg/scratch_msgs.js ../picoscratch-learn/web/msg -v
+
+cp blockly_compressed_vertical.js ../picoscratch/web -v
+cp blocks_compressed_vertical.js ../picoscratch/web -v
+cp blocks_compressed.js ../picoscratch/web -v
+#cp media ../picoscratch/web -rv
+cp msg/messages.js ../picoscratch/web/msg -v
+cp msg/scratch_msgs.js ../picoscratch/web/msg -v
